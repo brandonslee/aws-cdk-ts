c7146030d20f06f8a9eade8b90908e11e1a7fa8c
Brandon S. Lee <acidzazz@gmail.com>
2020-07-27 14:48:50 +0900

    Level1 Construct added for comparison with Level2

--- IAMRoleCreate/lib/iam_role_create-stack.ts
+++ IAMRoleCreate/lib/iam_role_create-stack.ts
@@ -6,6 +6,8 @@ export class IamRoleCreateStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    // Level2 Construct
     const role = new iam.Role(this, "acidBackendProcessingLambdaRole", {
       assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
       path: "/DigitalBooks/",
@@ -28,13 +30,38 @@ export class IamRoleCreateStack extends cdk.Stack {
 
     policy = new iam.Policy(this, "ReadFromUserImagesS3Bucket");
     policy.addStatements(new iam.PolicyStatement({
-        resources: ['arn:aws:s3:::*'],
-        actions: ['s3:Get*',
-          's3:List*',
-        ],
-        effect: iam.Effect.ALLOW,
+      resources: ['arn:aws:s3:::*'],
+      actions: ['s3:Get*',
+        's3:List*',
+      ],
+      effect: iam.Effect.ALLOW,
     }));
     role.attachInlinePolicy(policy);
+
+    policy = new iam.Policy(this, 'WriteToUserImagesS3Bucket');
+    policy.addStatements(new iam.PolicyStatement({
+    }));
+
+    // Level1 Construct
+    const role2 = new iam.CfnRole(this, "acidCloudWatchLogging", {
+      assumeRolePolicyDocument: {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Principal": {
+                    "Service": [
+                        "appsync.amazonaws.com"
+                    ]
+                },
+                "Action": [
+                    "sts:AssumeRole"
+                ]
+            }
+        ]
+      }
+
+    });
 
     
   }
