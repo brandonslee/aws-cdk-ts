8e7573fea2bb4a45bfd28ff3008d0a0484b1c28d
Brandon S. Lee <acidzazz@gmail.com>
2021-06-22 00:33:30 +0900

    eks cluster quick start

---
+++ eks-cluster-quick/lib/eks-cluster-quick-stack.ts
@@ -0,0 +1,30 @@
+import * as cdk from '@aws-cdk/core';
+import * as eks from '@aws-cdk/aws-eks';
+
+export class EksClusterQuickStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+    const cluster = new eks.Cluster(this, 'hello-eks', {
+      version: eks.KubernetesVersion.V1_20,
+    });
+
+    // apply a kubernetes manifest to the cluster
+    cluster.addManifest('mypod', {
+      apiVersion: 'v1',
+      kind: 'Pod',
+      metadata: { name: 'mypod' },
+      spec: {
+        containers: [
+          {
+            name: 'hello',
+            image: 'paulbouwer/hello-kubernetes:1.5',
+            ports: [ { containerPort: 8080 } ]
+          }
+        ]
+      }
+    });
+    
+  }
+}
